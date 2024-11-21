0d5100a6c47673f9ed2a5d1fc7bcf79c53a5a90b
PpWave <pplscream@gmail.com>
2024-11-22 01:10:30 +0300

    JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+    mode: 'development', // Установите 'development' для разработки или 'production' для производства
+    entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
