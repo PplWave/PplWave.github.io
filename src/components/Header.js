99e6f56ee9d76a845a5c830d07868afeb77c1167
PpWave <pplscream@gmail.com>
2024-11-24 04:43:13 +0300

    фиксы

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,35 @@
 import React from "react";
+import profilePic from "../images/profile-pic.jpeg"; 
 
 export default function Header() {
   return (
-    <div>
+    <div className="">
       <div className="header">
         <div className="profile-pic-box">
-          <img
-            src="/images/profile-pic.jpeg"
-            className="profile-pic"
-            alt="self-logo"
-          />
+        <img src={profilePic} className="profile-pic" alt="self-logo" />
         </div>
         <h2 className="profile-name">Сергей Евгеньевич</h2>
         <h4 className="role">QA Engineer</h4>
-        <a className="link" href="https://t.me/5819371109" target="_blank" rel="noreferrer">
+        <a className="link" href="https://web.telegram.org/a/#5819371109">
           Telegram
         </a>
       </div>
 
       <div className="d-flex justify-content-center bcard-buttons">
-        <a href="mailto:pada1one@mail.ru" className="btn btn-light" target="_blank" rel="noreferrer">
-          Email
-        </a>
-        <a
-          href="https://github.com/PplWave"
-          className="btn btn-light"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
+        <button className="btn btn-light">
+          <a href="mailto:pada1one@mail.ru" target="_blank" rel="noreferrer">
+            Email
+          </a>
+        </button>
+        <button className="btn  btn-light">
+          <a
+            href="https://github.com/PplWave" 
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </button>
       </div>
     </div>
   );
