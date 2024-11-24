07acd7b3e0686d8a8e6859b54a3708d854eb62c9
PpWave <pplscream@gmail.com>
2024-11-24 04:03:08 +0300

    устранение ошибок

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development', // Установите 'development' для разработки или 'production' для производства
-    entry: './src/index.js',
+  mode: 'development', // Установите 'development' для разработки или 'production' для производства
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '', // Убедитесь, что publicPath установлен правильно
+    publicPath: '/images/', // Убедитесь, что publicPath установлен правильно
   },
   module: {
     rules: [
